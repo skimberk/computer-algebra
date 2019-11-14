be85d27ded361e78dd2a5046e43c93fffc8e66e6
kimberks <skimberk1@gmail.com>
2019-11-14 17:34:57 -0600

    Got subtraction and multiplication working (I think)

--- bigint.c
+++ bigint.c
@@ -25,6 +25,11 @@ void freeBigInt(struct BigInt *x) {
     free(x);
 }
 
+void replaceBigInt(struct BigInt **x, struct BigInt *y) {
+    freeBigInt(*x);
+    x = &y;
+}
+
 void validateBigInt(struct BigInt *x) {
     if (x->sign != -1 && x->sign != 1) {
         printf("BigInt must have sign 1 or -1\n");
@@ -47,6 +52,11 @@ void validateBigInt(struct BigInt *x) {
     }
 }
 
+int isZeroBigInt(struct BigInt *x) {
+    validateBigInt(x);
+    return x->numBlocksUsed == 1 && x->blocks[0] == 0;
+}
+
 void growBigInt(struct BigInt *x) {
     x->numBlocks *= 2;
     uint32_t *newBlocks = malloc(x->numBlocks * sizeof(uint32_t));
@@ -57,7 +67,7 @@ void growBigInt(struct BigInt *x) {
     x->blocks = newBlocks;
 }
 
-int compareBigInt(struct BigInt *x, struct BigInt *y) {
+int compareAbsoluteBigInt(struct BigInt *x, struct BigInt *y) {
     validateBigInt(x);
     validateBigInt(y);
 
@@ -65,28 +75,24 @@ int compareBigInt(struct BigInt *x, struct BigInt *y) {
     unsigned int yBlocks = y->numBlocksUsed;
     unsigned int n = xBlocks > yBlocks ? xBlocks : yBlocks;
 
-    int withinBlocks;
     int currComp = 0;
-    int bothZero = 1;
     for (unsigned int i = 0; i < n; i++) {
-        withinBlocks = xBlocks > i && yBlocks > i;
-
-        if (bothZero && ((xBlocks > i && x->blocks[i] > 0) || (yBlocks > i && y->blocks[i] > 0))) {
-            bothZero = 0;
-        }
-
-        if ((yBlocks <= i && x->blocks[i] > 0) || (withinBlocks && x->blocks[i] > y->blocks[i])) {
+        if (yBlocks <= i || (xBlocks > i && x->blocks[i] > y->blocks[i])) {
             // Most signficant block of x (until now) is bigger than that of y
             currComp = 1;
-        } else if ((xBlocks <= i && y->blocks[i] > 0) || (withinBlocks && y->blocks[i] > x->blocks[i])) {
+        } else if (xBlocks <= i || (yBlocks > i && y->blocks[i] > x->blocks[i])) {
             // Same as previous case, but with x and y swapped (i.e. y > x)
             currComp = -1;
         }
     }
 
-    if (bothZero) {
-        return 0;
-    }
+    return currComp;
+}
+
+
+int compareBigInt(struct BigInt *x, struct BigInt *y) {
+    validateBigInt(x);
+    validateBigInt(y);
 
     if (x->sign > y->sign) {
         return 1;
@@ -96,9 +102,9 @@ int compareBigInt(struct BigInt *x, struct BigInt *y) {
         return -1;
     }
 
-    // If both x and y are negative, we want to flip currComp
+    // If both x and y are negative, we want to flip comparison of their absolute values
     // (at this point, we know their signs are equal)
-    return x->sign * currComp;
+    return x->sign * compareAbsoluteBigInt(x, y);
 }
 
 struct BigInt *addBigInt(struct BigInt *x, struct BigInt *y) {
@@ -107,31 +113,66 @@ struct BigInt *addBigInt(struct BigInt *x, struct BigInt *y) {
 
     struct BigInt *out = createBigInt(0);
 
-    unsigned int xBlocks = x->numBlocksUsed;
-    unsigned int yBlocks = y->numBlocksUsed;
-    unsigned int n = xBlocks > yBlocks ? xBlocks : yBlocks;
-
-    unsigned int carry = 0;
-
     // A subtraction problem (opposite signs)
     if (x->sign + y->sign == 0) {
-        int cmp = compareBigInt(x, y);
+        int cmp = compareAbsoluteBigInt(x, y);
         if (cmp == 0) {
             return out;
         }
 
         if (cmp < 0) {
+            // Ensure that x has larger absolute value
             struct BigInt *temp = x;
             x = y;
-            y = x;
+            y = temp;
         }
 
+        unsigned int xBlocks = x->numBlocksUsed;
+        unsigned int yBlocks = y->numBlocksUsed;
+        unsigned int n = xBlocks > yBlocks ? xBlocks : yBlocks;
+
+        uint32_t newBlock;
+        unsigned int borrow = 0;
         for (unsigned int i = 0; i < n; i++) {
+            if (xBlocks <= i) {
+                newBlock = y->blocks[i] - borrow;
+                borrow = 0;
+            } else if (yBlocks <= i) {
+                newBlock = x->blocks[i] - borrow;
+                borrow = 0;
+            } else {
+                newBlock = x->blocks[i] - y->blocks[i] - borrow;
+                if (newBlock > x->blocks[i]) {
+                    borrow = 1;
+                } else {
+                    borrow = 0;
+                }
+            }
 
+            if (newBlock != 0) {
+                // TODO: Do a better job with this
+                while (out->numBlocks <= i) {
+                    growBigInt(out);
+                }
+
+                while (out->numBlocksUsed <= i) {
+                    out->blocks[out->numBlocksUsed] = 0;
+                    out->numBlocksUsed++;
+                }
+                out->blocks[i] = newBlock;
+            }
         }
+
+        out->sign = x->sign;
+
         return out;
     }
     
+    unsigned int xBlocks = x->numBlocksUsed;
+    unsigned int yBlocks = y->numBlocksUsed;
+    unsigned int n = xBlocks > yBlocks ? xBlocks : yBlocks;
+
+    unsigned int carry = 0;
     for (unsigned int i = 0; i < n; i++) {
         if (out->numBlocks <= i) {
             growBigInt(out);
@@ -151,6 +192,8 @@ struct BigInt *addBigInt(struct BigInt *x, struct BigInt *y) {
             out->blocks[i] = x->blocks[i] + y->blocks[i] + carry;
             if (out->blocks[i] < x->blocks[i]) {
                 carry = 1;
+            } else {
+                carry = 0;
             }
         }
     }
@@ -164,6 +207,51 @@ struct BigInt *addBigInt(struct BigInt *x, struct BigInt *y) {
     }
     
     out->sign = x->sign;
+    return out;
+}
+
+struct BigInt *multiplyBigInt(struct BigInt *x, struct BigInt *y) {
+    validateBigInt(x);
+    validateBigInt(y);
+
+    struct BigInt *out = createBigInt(0);
+
+    unsigned int xBlocks = x->numBlocksUsed;
+    unsigned int yBlocks = y->numBlocksUsed;
+
+    uint32_t carry;
+    uint64_t result;
+    for (unsigned int i = 0; i < xBlocks; i++) {
+        carry = 0;
+        for (unsigned int j = 0; j < yBlocks; j++) {
+            if (i + j == out->numBlocksUsed) {
+                if (out->numBlocks == out->numBlocksUsed) {
+                    growBigInt(out);
+                }
+                out->numBlocksUsed++;
+            }
+
+            result = (uint64_t)x->blocks[i] * y->blocks[j];
+            out->blocks[i + j] = (uint32_t)result + carry;
+            carry = result / UINT32_MAX;
+
+            if (out->blocks[i + j] < (uint32_t)result) {
+                // Overflow during addition
+                carry++;
+            }
+        }
+        if (carry) {
+            if (i + yBlocks == out->numBlocksUsed) {
+                if (out->numBlocks == out->numBlocksUsed) {
+                    growBigInt(out);
+                }
+                out->numBlocksUsed++;
+            }
+            out->blocks[i + yBlocks] = carry;
+        }
+    }
+    
+    out->sign = x->sign * y->sign;
     return out;
 }
 
@@ -213,6 +301,40 @@ int main (int argc, char** argv) {
     for (int i = 0; i < 20; i++) {
         a = addBigInt(a, b);
         printBigInt(a); printf("\n");
+    }
+
+    b->sign = -1;
+	for (int i = 0; i < 40; i++) {
+        a = addBigInt(a, b);
+        printBigInt(a); printf("\n");
+    }
+    
+    b->sign = 1;
+	for (int i = 0; i < 40; i++) {
+        a = addBigInt(b, a);
+        printBigInt(a); printf("\n");
+    }
+    
+    b->sign = -1;
+    for (int i = 0; i < 40; i++) {
+        a = addBigInt(b, a);
+        printBigInt(a); printf("\n");
+    }
+
+    struct BigInt *d = createBigInt(2);
+    struct BigInt *e = createBigInt(2);
+    e->sign = -1;
+    for (int i = 0; i < 100; i++) {
+        d = multiplyBigInt(d, e);
+        printBigInt(d); printf("\n");
+    }
+
+    struct BigInt *f = createBigInt(1);
+    struct BigInt *g = createBigInt(0);
+    for (int i = 1; i < 101; i++) {
+        g->blocks[0] = i;
+        f = multiplyBigInt(f, g);
+        printBigInt(f); printf("\n");
     }
 
     freeBigInt(a);
